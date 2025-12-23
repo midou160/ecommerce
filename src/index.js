15f859da960671ba48ee36ccabf7a4d9f14b267f
Tabet Abdelhamid <mod359192@gmail.com>
2025-12-23 07:46:58 +0100

    product page done

--- src/index.js
+++ src/index.js
@@ -12,4 +12,20 @@ document.querySelectorAll(".add-to-cart").forEach(button => {
 })
 })
 
+document.querySelectorAll(".size-option input[type='radio']").forEach(item =>(
+    item.addEventListener("change", () => {
+        document.querySelectorAll(".size-option").forEach(i => i.classList.remove("active"))
+    
+     item.parentNode.parentNode.classList.add('active')
+    })
+))
+
+document.querySelectorAll(".color-option input[type='radio']").forEach(item =>(
+    item.addEventListener("change", () => {
+        document.querySelectorAll(".color-option").forEach(i => i.classList.remove("active"))
+    
+     item.parentNode.parentNode.classList.add('active')
+    })
+))
+
 document.getElementById("copyright").innerHTML = "جميع الحقوق محفوظة سنة " + new Date().getFullYear();
