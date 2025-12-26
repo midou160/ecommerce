d78969bd80e0a3b387566ebd842bc8ce62c914d8
Tabet Abdelhamid <mod359192@gmail.com>
2025-12-26 12:15:49 +0100

    cheackout page done

--- src/index.js
+++ src/index.js
@@ -28,4 +28,37 @@ document.querySelectorAll(".color-option input[type='radio']").forEach(item =>(
     })
 ))
 
+// حساب سعر اجمالي المنتج
+
+document.querySelectorAll('[data-product-quantity]').forEach(item => {
+    item.addEventListener("change", () => {
+        const newQuantity = item.value;
+        const parent = item.closest('[data-product-info]');
+        const pricePerUnit = parent.getAttribute('data-product-price');
+        const totalPriceforproduct = newQuantity * pricePerUnit;
+        parent.querySelector('.total-price-for-product').innerHTML = totalPriceforproduct+'$';
+
+        calculateTotalPrice();
+})});
+
+document.querySelectorAll('[data-remove-from-card]').forEach(item => {
+    item.addEventListener("click", () => {
+        const parent = item.closest('[data-product-info]');
+        parent.remove();
+        calculateTotalPrice();
+       
+    })
+})
+
+function calculateTotalPrice() {
+     let totalPriceforAllproduct = 0;
+        document.querySelectorAll('[data-product-info]').forEach(product => {
+            const pricePerUnit = product.getAttribute('data-product-price');
+            const quantity = product.querySelector('[data-product-quantity]').value;
+            const totalPriceforproduct = pricePerUnit * quantity;
+            totalPriceforAllproduct += totalPriceforproduct;
+        }
+        )
+        document.querySelector('#total-price-for-all-products').innerHTML = totalPriceforAllproduct+'$';
+}
 document.getElementById("copyright").innerHTML = "جميع الحقوق محفوظة سنة " + new Date().getFullYear();
--- webpack.config.js
+++ webpack.config.js
@@ -75,14 +75,6 @@ module.exports = {
   },
 
   plugins: [
-    new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: './src/index.html'
-    }),
-     new HtmlWebpackPlugin({
-      filename: 'product.html',
-      template: './src/product.html'
-    }),
     new MiniCssExtractPlugin({
       filename: 'css/style.css'
     }),
